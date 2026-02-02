deffa2a62f1308797debf003ef1ac0c6fd22c014
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-02-02 17:06:18 +0100

    revert forbidNonWhitelisted due to frontend login issues

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: 'http://localhost:3000', // frontend URL
+    credentials: true, // if you plan to send cookies
   });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3333);
 }
 bootstrap();
