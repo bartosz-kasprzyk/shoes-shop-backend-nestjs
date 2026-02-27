ace9a745c64c2cc46927e30c89dc5546797e8af3
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-02-27 21:27:20 +0100

    update port binding and CORS for Render deployment

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-  origin: (origin, callback) => {
-    // Allow all origins for now to troubleshoot
-    callback(null, true);
-  },
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204, // Some browsers hang if they don't get a 204 here
-});
+    origin: (origin, callback) => {
+      // Allow all origins for now to troubleshoot
+      callback(null, true);
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204, // Some browsers hang if they don't get a 204 here
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3333);
+  const port = process.env.PORT || 3333;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
