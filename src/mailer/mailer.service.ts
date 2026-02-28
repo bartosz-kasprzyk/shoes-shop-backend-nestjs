c969997469b7c00ede58d9124f961cf627922f23
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2026-02-28 18:00:52 +0100

    add console logs for troubleshooting

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -6,15 +6,29 @@ export class MailerServiceImplementation {
   constructor(private readonly mailerService: MailerService) {}
 
   async sendConfirmationEmail(email: string, token: string) {
+    console.log('--- DEBUG RENDERA ---');
+    console.log('User:', process.env.MAIL_USER);
+    console.log(
+      'C-ID:',
+      process.env.GOOGLE_CLIENT_ID?.substring(0, 10) + '...',
+    );
+    console.log('Refresh Token obecny?:', !!process.env.GOOGLE_REFRESH_TOKEN);
+    console.log('--- END DEBUG ---');
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:3333';
     const url = `${backendUrl}/api/auth/local/confirm?token=${token}`;
 
-    await this.mailerService.sendMail({
-      to: email,
-      subject: 'Activate your Shoes Shop account! 👟',
-      template: './confirmation',
-      context: { url },
-    });
+    try {
+      await this.mailerService.sendMail({
+        to: email,
+        subject: 'Activate your Shoes Shop account! 👟',
+        template: './confirmation',
+        context: { url },
+      });
+      console.log('✅ Email sent successfully to:', email);
+    } catch (error) {
+      console.error('❌ MAILER ERROR:', error.message);
+      // This will print the real reason in your terminal!
+    }
   }
 
   async sendPasswordResetEmail(email: string, code: string) {
@@ -24,7 +38,7 @@ export class MailerServiceImplementation {
       to: email,
       subject: 'Reset your password - Shoes Shop',
       template: './reset-password',
-      context: { url }
+      context: { url },
     });
   }
 }
